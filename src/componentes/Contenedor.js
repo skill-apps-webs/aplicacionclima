b85303cc404ee33c7b94dfbfe2fb8fe30e1c1c9a
vane <nunezramirez20@gmail.com>
2019-05-25 19:25:14 -0500

    agregado autor

--- src/componentes/Contenedor.js
+++ src/componentes/Contenedor.js
@@ -7,14 +7,17 @@ import ClimaDetalle from "./ClimaDetalle";
  */
 const Contenedor = () => (
  <div className="contenedor">
-    <div> Aplicacion del Tiempo</div>
+    <div> Linea de Tiempo</div>
     <div className="contenido">   
         <ListadoLocalidad></ListadoLocalidad>
         <ClimaDetalle></ClimaDetalle>
-
+        </div>
+     <div> 
+         creado en el lab 3 
+         </div>   
     </div>
-</div>
-     
+
+
 );
   
 export default Contenedor;
