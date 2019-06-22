107cc9e9720d75506dece6298b06282ff5cdfe3f
felix apaza <felix.apaza@gmail.com>
2019-06-22 16:21:33 -0500

    Listando component con map

--- src/componentes/ListadoLocalidad.js
+++ src/componentes/ListadoLocalidad.js
@@ -1,12 +1,33 @@
 import React, {Component} from "react";
 import ClimaLocalidad from "./ClimaLocalidad";
+import { CombineLatestOperator } from "rxjs/internal/observable/combineLatest";
 
 /**
  * Felix
  */
+const arregloLocalidades = [
+    {id:1, nombreciudad:'Lima',temp:15},
+    {id:2, nombreciudad:'Santiago',temp:12},
+    {id:3, nombreciudad:'bogota', temp:30},
+    {id:4, nombreciudad:'rio de janeiro', temp:33},
+    {id:5, nombreciudad:'caracas', temp:33},
+    {id:6, nombreciudad:'new york', temp:25},
+    {id:7, nombreciudad:'japon', temp:13},
+    {id:8, nombreciudad:'montevideo', temp:18},
+    
+]
+
 class ListadoLocalidad extends Component {
 
+    constructor(){
+        super()
+        this.state = {
+            listaLocalidades:arregloLocalidades
+        }
+    }
+
     render(){
+        const {listaLocalidades} = this.state;
         return (
             <div className="listadolocalidad">
                 <div>
@@ -16,14 +37,9 @@ class ListadoLocalidad extends Component {
                     Listado de ciudades con su temperatura
                 </div>
                 <div>
-                    <ClimaLocalidad nombreciudad="Lima" temp={10} ></ClimaLocalidad>
-                    <ClimaLocalidad nombreciudad="Santiago" temp={12}></ClimaLocalidad>
-                    <ClimaLocalidad nombreciudad="bogota" temp={30}></ClimaLocalidad>
-                    <ClimaLocalidad nombreciudad="rio de janeiro" temp={33}></ClimaLocalidad>
-                    <ClimaLocalidad nombreciudad="caracas"temp={33}></ClimaLocalidad>
-                    <ClimaLocalidad nombreciudad="new york" temp={25}></ClimaLocalidad>
-                    <ClimaLocalidad nombreciudad="japon" temp={13}></ClimaLocalidad>
-                    <ClimaLocalidad nombreciudad="montevideo" temp={18}></ClimaLocalidad>
+                    {listaLocalidades.map((localidad, index)=>{
+                        return <ClimaLocalidad key={localidad.id} nombreciudad={localidad.nombreciudad} temp={localidad.temp} />
+                    })}
                 </div>
             
             </div>
@@ -32,3 +48,4 @@ class ListadoLocalidad extends Component {
     }
 }
 export default ListadoLocalidad;  
+
